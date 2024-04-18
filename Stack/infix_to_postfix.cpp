276a4016891e878aba80879cb402cef65d565fd5
Rakibhasan-programmer <rakibulhasan.seu.cse@gmail.com>
2024-04-18 08:11:50 +0600

    infix to postfix

---
+++ Stack/infix_to_postfix.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int prec(char ch){
+    if(ch == '+' || ch == '-') return 0;
+
+    return 1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    stack<char> st;
+    string ans = "";
+
+    for(int i=0; i<s.size(); i++){
+        char now = s[i];
+
+        if(now>='a' && now<='z'){
+            ans+=now;
+        }else{
+            while(st.size() && prec(st.top()) >= prec(now)){
+                ans+=st.top();
+                st.pop();
+            }
+            st.push(now);
+        }
+    }
+    while(st.size()){
+        ans+=st.top();
+        st.pop();
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
