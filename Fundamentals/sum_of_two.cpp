a2a72114be4bbe43d971f9c3df155c6292fc9dbf
rakibulhasan-swe <rakibulhasan.seu.cse@gmail.com>
2026-07-27 14:48:28 +0600

    readme md edited with examples

---
+++ Fundamentals/sum_of_two.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << "Sum of two = " << a+b << "\n";
+
+    return 0;
+}
